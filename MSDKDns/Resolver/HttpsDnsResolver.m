17a91540bb18acb6291047b77fa0ad0bcef98eb8
erichuyuehu <erichuyuehu@tencent.com>
2024-09-26 17:26:41 +0800

    feat:删除多余日志

--- MSDKDns/Resolver/HttpsDnsResolver.m
+++ MSDKDns/Resolver/HttpsDnsResolver.m
@@ -88,7 +88,6 @@ static NSURLSession *_resolveHOSTSession = nil;
 }
 
 - (void)startDataTaskWithHttpDnsUrl:(NSURL *)httpDnsUrl domains:(NSArray *)domains timeOut:(float)timeOut delegate:(id<MSDKDnsResolverDelegate>)delegate {
-    MSDKDNSLOG("HttpDns Request domains: %@", domains);
     MSDKDNSLOG("HttpDns Request URL: %@", httpDnsUrl);
     NSURLRequest *request = [NSURLRequest requestWithURL:httpDnsUrl
                                                cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
